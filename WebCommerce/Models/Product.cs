f7b2a7ad8deae1a61644c5a2aaf11a612d544956
KhaiLeHuynh <khaile.huynhduy@gmail.com>
2024-08-07 10:06:09 +0700

    addForgeignKey

--- WebCommerce/Models/Product.cs
+++ WebCommerce/Models/Product.cs
@@ -8,14 +8,20 @@ namespace WebCommerce.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ProductID { get; set; }
-        [Required]
-        public string? ProductName { get; set; }
-        [Required]
-        public string? Description { get; set; }
-        
-        public string? CategoryName { get; set; }
-        [Required]
-        public double? ProductPrice { get; set; }
 
+        [Required]
+        public string ProductName { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public double ProductPrice { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
     }
 }
