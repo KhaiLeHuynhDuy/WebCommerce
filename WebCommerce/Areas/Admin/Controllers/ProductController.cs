5c789615442db403bb292f14374763ab745ee09d
KhaiLeHuynh <khaile.huynhduy@gmail.com>
2024-08-07 08:22:03 +0700

    add product curd

--- WebCommerce/Areas/Admin/Controllers/ProductController.cs
+++ WebCommerce/Areas/Admin/Controllers/ProductController.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebCommerce.Models;
+using WebCommerce.Repository.IRepository;
 
 namespace WebCommerce.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class ProductController : Controller
     {
+        private readonly IProductRepository _productRepository;
+        public ProductController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Product> products = _productRepository.GetAll().ToList();
+            return View(products);
         }
     }
 }
